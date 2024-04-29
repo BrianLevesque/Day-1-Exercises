b3656ce696f60d8aac2041e83aa998293d3d4380
Brian Levesque <kaka10@comcast.net>
2024-04-29 15:45:20 -0400

    initial commit

---
+++ Day1_Excercies.py
@@ -0,0 +1,45 @@
+#EXERCISE 1: Echo String
+
+text = input("Enter some text:")
+print(text)
+
+#EXERCISE 2: Adding a number to an integer
+
+
+num = int(input("Enter a number:"))
+output = num + 1
+print(output)
+
+#EXERCISE 3: Adding a number to a float
+
+flt = float(input("Enter a number:"))
+output = flt + .5
+print(output)
+
+#EXERCISE 4: Adding Two Floats
+
+
+
+flt = float(input("Enter a number:"))
+flt2 = float(input("Enter another number:"))
+sum = flt + flt2
+
+print("The sum is",sum)
+
+
+
+#EXERCISE 5: Multiplying Floats
+
+flt = float(input("Enter a number:"))
+flt2 = float(input("Enter another number:"))
+product = flt * flt2
+print("The product is",product)
+
+#EXERCISE 6: Dividing Integers
+
+num1 = int(input("Enter a number:"))
+num2 = int(input("Enter another number:"))
+
+result = int(num1 / num2)
+
+print("The result is", result)
